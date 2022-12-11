66be478fdde8ec09176f0e7947752083582faf6d
Ryszard Knop <dragoon@dragonic.eu>
2022-12-12 00:41:31 +0100

    Add support for formatting labels/help_texts in dynamic fields
    Add label_/help_text_type = text/html/markdown and off you go!

--- events/dynaforms/fields.py
+++ events/dynaforms/fields.py
@@ -1,9 +1,11 @@
 from abc import ABC
 from typing import Literal, Optional, Union, ClassVar, Annotated
 
+import markdown
 from django.forms.fields import Field
 from django.forms.widgets import Widget
 from django.forms import fields, widgets
+from django.utils.safestring import mark_safe
 from phonenumber_field import formfields as pnfields
 
 from pydantic import validator, BaseModel, Field as PydanticField
@@ -15,8 +17,12 @@ class DynaformField(BaseModel, ABC):
     _widget: ClassVar[Widget] = None
 
     label: str
-    required: bool = True
+    label_type: str = 'text'
+
     help_text: Optional[str] = None
+    help_text_type: str = 'text'
+
+    required: bool = True
 
 
 class ChoiceField(DynaformField, ABC):
@@ -107,6 +113,37 @@ def dynaform_prefix(name: Optional[str]) -> str:
         return ""
 
 
+def parse_text_type_transform(config: dict, text_key: str, type_key: str) -> None:
+    if not (text_type := config.get(type_key)):
+        return
+
+    # We should not pass this to the field constructor:
+    del config[type_key]
+
+    if not (text := config.get(text_key)):
+        return
+
+    if text_type == 'text':
+        pass
+    elif text_type == 'html':
+        text = mark_safe(text)
+    elif text_type == 'markdown':
+        rendered = markdown.markdown(text, output_format='html5')
+
+        # A quick and somwhat hacky way to strip the wrapping
+        # <p> element, but only if there's a single <p> in the text.
+        # This is not fully compliant, but much faster than building
+        # the full HTML tree, then serializing it back to text.
+        if text.find("</p>") == text.rfind("</p>"):
+            rendered = rendered.removeprefix("<p>").removesuffix("</p>")
+
+        text = mark_safe(rendered)
+    else:
+        raise ValueError(f"{type_key} is not one of text/html/markdown!")
+
+    config[text_key] = text
+
+
 def dynaform_to_fields(name: Optional[str], template: str) -> dict[str, Field]:
     config = DynaformFieldConfiguration.parse_raw(template)
     prefix = dynaform_prefix(name)
@@ -124,6 +161,9 @@ def dynaform_to_fields(name: Optional[str], template: str) -> dict[str, Field]:
 
         if 'choices' in config_args:
             config_args['choices'] = transform_choices(config_args['choices'])
+
+        parse_text_type_transform(config_args, 'label', 'label_type')
+        parse_text_type_transform(config_args, 'help_text', 'help_text_type')
 
         dynamic_fields[prefix + field_name] = field_config._field_class(**config_args)
 
