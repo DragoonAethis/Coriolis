b82e6f49d9cba41ba55c800a472e73a71fc09a02
Ryszard Knop <dragoon@dragonic.eu>
2022-12-29 16:39:41 +0100

    Move custom AdminSite to the app package

--- coriolis/apps.py
+++ coriolis/apps.py
@@ -2,4 +2,4 @@ from django.contrib.admin.apps import AdminConfig
 
 
 class CoriolisAdminConfig(AdminConfig):
-    default_site = "coriolis.admin.AdminSite"
+    default_site = "events.admin.site.AdminSite"
