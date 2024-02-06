a4329b36d3a86bcd3fa8d4808557b5248b6303e8
Ryszard Knop <dragoon@dragonic.eu>
2024-02-06 01:17:09 +0100

    Reformat + ruff check coriolis-render.py

--- contrib/ticket-renderer/coriolis-render.py
+++ contrib/ticket-renderer/coriolis-render.py
@@ -1,12 +1,11 @@
-import os
 import json
+import os
 
+from jinja2 import Environment, select_autoescape
 from playwright.sync_api import sync_playwright
-from jinja2 import Environment, PackageLoader, select_autoescape
 
-TICKET_WIDTH = int(os.environ['TICKET_WIDTH'])
-TICKET_HEIGHT = int(os.environ['TICKET_HEIGHT'])
-
+TICKET_WIDTH = int(os.environ["TICKET_WIDTH"])
+TICKET_HEIGHT = int(os.environ["TICKET_HEIGHT"])
 
 with open("render.html.j2") as f:
     env = Environment(autoescape=select_autoescape())
@@ -15,18 +14,20 @@ with open("render.html.j2") as f:
 with open("render.json") as f:
     template_params = json.load(f)
 
-with open("render.html", 'w') as f:
+with open("render.html", "w") as f:
     f.write(template.render(template_params) + "ASDF")
 
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
-    page.set_viewport_size({
-        "width": TICKET_WIDTH,
-        "height": TICKET_HEIGHT,
-    })
+    page.set_viewport_size(
+        {
+            "width": TICKET_WIDTH,
+            "height": TICKET_HEIGHT,
+        }
+    )
 
-    page.goto(f"file://{os.getcwd()}/render.html", wait_until='load')
+    page.goto(f"file://{os.getcwd()}/render.html", wait_until="load")
 
     # For whatever reason, the screenshot might be taken before
     # Chromium finishes the page paint, which usually results in
@@ -38,11 +39,14 @@ with sync_playwright() as p:
     # This should not happen, but I don't have any better ideas.
     page.wait_for_timeout(100)
 
-    page.screenshot(path="render.png", clip={
-        "x": 0,
-        "y": 0,
-        "width": TICKET_WIDTH,
-        "height": TICKET_HEIGHT,
-    })
+    page.screenshot(
+        path="render.png",
+        clip={
+            "x": 0,
+            "y": 0,
+            "width": TICKET_WIDTH,
+            "height": TICKET_HEIGHT,
+        },
+    )
 
     browser.close()
