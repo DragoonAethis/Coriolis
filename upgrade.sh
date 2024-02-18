4d8dd203be59d285f9b2d551afd318474cb86445
Ryszard Knop <dragoon@dragonic.eu>
2024-02-18 19:35:12 +0100

    Don't restart the service socket after restarting the associated service

--- upgrade.sh
+++ upgrade.sh
@@ -9,7 +9,7 @@ poetry run python manage.py migrate
 poetry run python manage.py compilemessages
 poetry run python manage.py collectstatic --no-input
 sudo chown -R www-data:www-data .
-sudo systemctl restart nginx.service coriolis.service coriolis.socket coriolis-crontab.service coriolis-dramatiq.service
+sudo systemctl restart nginx.service coriolis.service coriolis-crontab.service coriolis-dramatiq.service
 
 pushd /app/contrib/ticket-renderer
 ./build-image.sh r2024
